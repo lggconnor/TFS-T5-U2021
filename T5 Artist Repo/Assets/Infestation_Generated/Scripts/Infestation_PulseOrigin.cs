4f6f52604bce659b5f80493311558f7c6cdf1904
shawnlanda <shawnlanda28@gmail.com>
2021-07-19 11:25:29 -0400

    Infestation prefabs added

---
+++ T5 Artist Repo/Assets/Infestation_Generated/Scripts/Infestation_PulseOrigin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Infestation_PulseOrigin : MonoBehaviour
+{
+
+    // Fetch the shader's emission origin variable
+
+    public Material RefMaterial;
+
+    void Update()
+    {
+        RefMaterial.SetVector("_EmissionOrigin", transform.position);
+    }
+
+}

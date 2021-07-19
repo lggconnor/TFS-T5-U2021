4c5a535ede499597a9e881913501826f9caf9f83
lggconnor <connor@littleguygames.com>
2021-07-19 10:50:52 -0400

    changes from class 07-19

---
+++ T5 Artist Repo/Assets/RotateThis.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateThis : MonoBehaviour
+{
+    public float rotateSpeed = 0.1f;
+    public Vector3 rotateDir;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(rotateDir * rotateSpeed);
+    }
+}
